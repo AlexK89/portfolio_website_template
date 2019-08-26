3b04a730d08d0f6ab33cf44e975edcd19550c79a
alexk89 <kozhushko.vasyl@gmail.com>
2019-08-26 13:49:18 +0100

    scroll to top

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React, { useState, useEffect } from 'react'
-import { Switch, Route } from 'react-router-dom'
+import { HashRouter, Switch, Route } from 'react-router-dom'
 import Loader from './components/Loader/Loader'
 import { URL } from './helpers/static'
 import { db } from './helpers/db'
@@ -22,28 +22,30 @@ function App() {
   }, [])
 
   const { details, about, portfolio, projects } = content
-  
+
   return (
     details ?
       <div className="App">
-        <Navigation details={details} />
+        <HashRouter basename={URL}>
+          <Navigation details={details} />
 
-        <Switch>
-          <Route path={`${URL}/`} exact component={() =>
-            (about && projects) ? <About about={about} projects={projects} /> : <Loader />
-          } />
-          <Route path={`${URL}/portfolio`} exact component={() =>
-            (portfolio && projects) ? <Portfolio portfolio={portfolio} projects={projects} /> : <Loader />
-          } />
-          <Route path={`${URL}/portfolio/project/:projectId`} component={(props) =>
-            projects ? <Project projects={projects} projectId={props.match.params.projectId} />
-              : <Loader />
-          } />
-        </Switch>
+          <Switch>
+            <Route path={`${URL}/`} exact component={() =>
+              (about && projects) ? <About about={about} projects={projects} /> : <Loader />
+            } />
+            <Route path={`${URL}/portfolio`} exact component={() =>
+              (portfolio && projects) ? <Portfolio portfolio={portfolio} projects={projects} /> : <Loader />
+            } />
+            <Route path={`${URL}/portfolio/project/:projectId`} component={(props) =>
+              projects ? <Project projects={projects} projectId={props.match.params.projectId} />
+                : <Loader />
+            } />
+          </Switch>
 
-        <footer>
-          <p>&copy; Kristina Kozhushko | <a href="http://www.codersinhoods.dev">CodersInHoods</a></p>
-        </footer>
+          <footer>
+            <p>&copy; Kristina Kozhushko | <a href="http://www.codersinhoods.dev">CodersInHoods</a></p>
+          </footer>
+        </HashRouter>
       </div>
       : <Loader />
   )
--- src/HOC/ScrollToTop.js
+++ src/HOC/ScrollToTop.js
@@ -1,11 +1,7 @@
-import { useEffect } from 'react'
 import { withRouter } from "react-router-dom";
 
-const ScrollToTop = ({ children, location: { pathname } }) => {
-    useEffect(() => {
-        window.scrollTo(0, 0);
-    }, [pathname]);
-
+const ScrollToTop = ({ children }) => {
+    window.scrollTo(0, 0);
     return children || null;
 };
 
